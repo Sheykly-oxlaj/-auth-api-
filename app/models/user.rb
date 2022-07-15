dd4407de6fbb71178010b4750905750ed73f1061
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-14 22:11:38 -0700

    adds two POST request and corrects a mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  def authenticate_user
-    has_secure_password
-    validates :email, presence: true, uniqueness: true
-  end
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
